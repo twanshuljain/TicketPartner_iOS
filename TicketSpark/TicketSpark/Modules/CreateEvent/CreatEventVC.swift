fea81f56bf70e4083eed6a2510bae35953340177
twAshish07 <ashish+1@thoughtwin.com>
2024-02-13 12:22:56 +0530

    CreateEvent

---
+++ TicketSpark/TicketSpark/Modules/CreateEvent/CreatEventVC.swift
@@ -0,0 +1,18 @@
+//
+//  CreatEventVC.swift
+//  TicketSpark
+//
+//  Created by Apple on 12/02/24.
+//
+
+import UIKit
+
+class CreatEventVC: UIViewController {
+    @IBOutlet weak var txtFld: CustomTextFieldView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
